f5295b787d307406ef03ddf2422d9e2bb3dc856c
GotoTatsuki <tokyo.hal.ths70632@gmail.com>
2019-05-28 13:58:11 +0900

    no message

---
+++ Assets/Scripts/game/Gimmick/Pillar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour
+{
+
+    public Pres_Pillar press_pillar;
+    private bool press;         //下がり上がりフラグ
+    private float counter;
+    private float i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        press = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (press == false)
+        {
+            if (i < press_pillar.Get_Fall)
+            {
+                i += press_pillar.Get_Speed;
+                transform.localScale += new Vector3(0.0f, press_pillar.Get_Speed, 0.0f);
+            }
+            Debug.Log(press_pillar.Get_Fall);
+            if (i >= press_pillar.Get_Fall)
+            {
+                //transform.position += new Vector3(0.0f, 0.0f, 0.0f);
+                counter++;
+                if (counter > press_pillar.Get_Interval)
+                {
+                    press = true;
+                    counter = 0;
+                    i = 0;
+                }
+            }
+        }
+
+        if (press == true)
+        {
+            if (i < press_pillar.Get_Fall)
+            {
+                i += press_pillar.Get_Speed;
+                transform.localScale += new Vector3(0.0f, -press_pillar.Get_Speed, 0.0f);
+            }
+            if (i >= press_pillar.Get_Fall)
+            {
+                //transform.position += new Vector3(0.0f, 0.0f, 0.0f);
+                counter++;
+                if (counter > press_pillar.Get_Interval)
+                {
+                    press = false;
+                    counter = 0;
+                    i = 0;
+                }
+            }
+        }
+    }
+
+    public bool Get_Press { get { return press; } }
+
+    public float Get_Cnt { get { return i; } }
+}
---
+++ Assets/Scripts/game/Gimmick/conveyor_left.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conveyor_left : MonoBehaviour
+{
+    private float m_uvSpeed = -0.5f;
+    private float m_movePower = 300.0f;
+    GameObject player;
+    /// <summary>
+    /// テクスチャのUV値をスクロールさせて、ベルトコンベアの見た目を表現する
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var material = GetComponent<Renderer>().material;
+        Vector2 offset = material.mainTextureOffset;
+        offset += Vector2.up * m_uvSpeed * Time.deltaTime;
+        material.mainTextureOffset = offset;
+    }
+
+    //void OnCollisionEnter(Collision other)
+    //{
+    //    var body = player.gameObject.GetComponent<Rigidbody>();
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Vector3 addPower = transform.forward * m_movePower;
+    //        body.AddForce(addPower, ForceMode.Acceleration);
+    //    }
+    //}
+
+    //void OnCollisionExit(Collision other)
+    //{
+    //    var body = other.gameObject.GetComponent<Rigidbody>();
+    //    if (body != null)
+    //    {
+    //        Vector3 addPower = -transform.forward * m_movePower;
+    //        body.AddForce(addPower, ForceMode.Acceleration);
+    //    }
+    //}
+
+    void OnCollisionEnter(Collision other)
+    {
+        var body = other.gameObject.GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            Vector3 addPower = -transform.right * m_movePower;
+            body.AddForce(addPower, ForceMode.Acceleration);
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        var body = other.gameObject.GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            Vector3 addPower = -transform.forward * m_movePower;
+            body.AddForce(addPower, ForceMode.Acceleration);
+        }
+    }
+
+}
